d3368f94416929fadaf5abe34fdb050162e3b662
PavelPotapov <pavelpotapovv96@gmail.com>
2024-06-21 01:28:55 +0500

    add slider

--- src/widgets/audioPlayer/AudioPlayer.tsx
+++ src/widgets/audioPlayer/AudioPlayer.tsx
@@ -1,24 +1,73 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import audioStore from '@/app/store/AudioStore'
+import { Progress, Slider } from '@vkontakte/vkui' // Подставьте корректные импорты из вашего UI-фреймворка
 
 const AudioPlayer: React.FC = observer(() => {
   const audioRef = useRef<HTMLAudioElement | null>(null)
+  const [isDragging, setIsDragging] = useState(false) // Состояние для отслеживания перетаскивания ползунка
 
   useEffect(() => {
     if (audioRef.current) {
       audioStore.setAudio(audioRef.current)
     }
 
-    // Очистка слушателей при размонтировании
     return () => {
-      if (audioRef.current) {
-        audioStore.removeAudio() // Добавляем метод для удаления аудио элемента из хранилища
-      }
+      audioStore.removeAudio()
     }
   }, [])
 
-  return <audio ref={audioRef} />
+  const formatTime = (time: number): string => {
+    const minutes = Math.floor(time / 60)
+    const seconds = Math.floor(time % 60)
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+  }
+
+  const handleSliderChange = (value: number) => {
+    if (!isDragging) {
+      audioStore.setCurrentTime(value) // Устанавливаем текущее время только если не идет перетаскивание
+    }
+  }
+
+  const handleSliderDragStart = () => {
+    setIsDragging(true) // Устанавливаем флаг начала перетаскивания
+  }
+
+  const handleSliderDragEnd: React.DragEventHandler<HTMLDivElement> = event => {
+    if (audioRef.current && event.currentTarget instanceof HTMLDivElement) {
+      const rect = event.currentTarget.getBoundingClientRect()
+      const offsetX = event.clientX - rect.left
+      const percentage = offsetX / rect.width
+      const newValue = percentage * audioStore.duration
+
+      audioStore.setCurrentTime(newValue) // Устанавливаем текущее время по окончании перетаскивания
+    }
+
+    setIsDragging(false) // Сбрасываем флаг по окончании перетаскивания
+  }
+
+  return (
+    <div>
+      <audio ref={audioRef} />
+      {audioStore.currentSong && (audioStore.isPlaying || !audioStore.isPlaying) && (
+        <>
+          <Slider
+            value={audioStore.currentTime}
+            min={0}
+            max={audioStore.duration}
+            onChange={handleSliderChange}
+            onDragStart={handleSliderDragStart}
+            onDragEnd={handleSliderDragEnd}
+            style={{ width: '100%' }}
+          />
+        </>
+      )}
+      {audioStore.isLoading && <p>Loading...</p>}
+      <p>
+        {formatTime(audioStore.currentTime)} / {formatTime(audioStore.duration)}
+      </p>
+    </div>
+  )
 })
 
 export default AudioPlayer
